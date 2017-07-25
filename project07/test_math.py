a3c7da55da550c0277a74c7a5bdb6e92d4402dd1
vladvillalopez <vladimi.villanueva@upr.edu>
2017-07-25 16:24:47 -0400

    update travis

--- project07/test_math.py
+++ project07/test_math.py
@@ -8,7 +8,8 @@ import pytest
 def test_add():
     assert fcm.math.add(5,2) == 7
     assert fcm.math.add(2,5) == 7
-
+    assert fcm.math.add(1,3) == 3
+#testdata =[(2, 5, 10),(1, 2, 2),(5)]
 def test_mult():
     assert fcm.math.mult(5,2) == 10
     assert fcm.math.mult(2,5) == 10
@@ -16,4 +17,3 @@ def test_mult():
 def test_div():
     assert fcm.math.div(8,2) == 4
     assert fcm.math.div(10,2) == 5
-    
